87cb634bf7b6dc391ce10f9b3f32eb6eef05d8fb
Emily Parkes <emilycoco@me.com>
2017-11-29 15:29:43 +1300

    check if works

--- minesweeper.js
+++ minesweeper.js
@@ -2,40 +2,88 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-  cells: []
-}
-
-function makeBoard(size){
-  for(i = 0; i <= size; i++){
-    for (j=0;j<=size; j++){
-    board.cells.push(
-      {row:i,
-        col:j,
-        isMine:Math.random() < 0.2,
-        hidden:true,
-        isMarked:false,
-        surroundingMines:0
-      })
+  cells:  [
+    {
+      row: 0,
+      col: 0,
+      hidden: true,
+      isMarked: false,
+      isMine: true,
+      surroundingMines: 0
+    },
+    {
+      row: 0,
+      col: 1,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 0,
+      col: 2,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 0,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 1,
+      hidden: true,
+      isMarked: false,
+      isMine: true,
+      surroundingMines: 0
+    },
+    {
+      row: 1,
+      col: 2,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 0,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 1,
+      hidden: true,
+      isMarked: false,
+      isMine: false,
+      surroundingMines: 0
+    },
+    {
+      row: 2,
+      col: 2,
+      hidden: true,
+      isMarked: false,
+      isMine: true,
+      surroundingMines: 0
     }
-  }
-}
+  ]
 
- function randomNumber(minimum, maximum) {
-   return Math.round (Math.random() * (maximum - minimum) + minimum)
- }
+}
 
 function startGame () {
+  for (var i = 0; i < board.cells.length; i++ ) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
 
-  makeBoard(randomNumber(2, 5))
-  //count mines surrounding
-  let cell = 0
-    for (cell in board.cells) {
-      board.cells[cell].surroundingMines = countSurroundingMines(cell)
-  }
-
-  document.addEventListener('click', checkForWin)
- document.addEventListener('contextmenu', checkForWin)
-
+    document.addEventListener('click', checkForWin)
 
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
@@ -49,43 +97,22 @@ function startGame () {
 
 function checkForWin () {
 
-  //for each cell
-  let cell = 0
-    for (cell in board.cells) {
-//if the cell is a mine but is not marked then start process again
-if (board.cells[cell].isMine){
-  if(!board.cells[cell].isMarked){
-    return
+//for each cell, check conditions for winning:
+for (let cell in board.cells) {
+  if (board.cells[cell].isMine){ // if it's a mine
+    if (!(board.cells[cell].hidden)) { // but it's not marked
+      return;
+    }
+  }
+  if (board.cells[cell].hidden) { //there's a hidden cell
+    if (!(board.cells[cell].isMine)) // which isn't a mine
+    return;
   }
 }
-
-//if the cell is not a mine and is still not revealed then start again
-if(board.cells[cell].hidden){
-  if(!board.cells[cell].isMine){
-    return
-  }
-}
-
-}
-
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  // if the cells all pass the above conditions then display =
-  lib.displayMessage('You Win! Push up on Keyboard to restart with same board size! Push down to try a new size! ')
-
-  function restartGameNew(){
-    window.location.reload(true)
-  }
-  function restartGameSame(){
-    window.location.reload(false)
-  }
-
-  document.addEventListener('keydown', restartGameNew)
-  document.addEventListener('keyup', restartGameSame)
-
-
-
- }
+     lib.displayMessage('You win!')
+}
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -96,16 +123,13 @@ if(board.cells[cell].hidden){
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
-  var surrounding= lib.getSurroundingCells(
-     board.cells[cell].row,
-     board.cells[cell].col
-   )
-   let count = 0
-   let curCell = 0
-   for (curCell in surrounding) {
-     if (surrounding[curCell].isMine) {
-       count++
-     } // if
-   } // for
-   return count
- }
+  var count = 0
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+
+  for (var i = 0; i < surrounding.length; i++) {
+    if (surrounding[i].isMine) {
+      count++
+    }
+  }
+  return count
+}
